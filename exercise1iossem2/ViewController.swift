0e51265b03ad8953f00665e335d71053345cc048
simranjeetkaur <chakkalsimmi@gmail.com>
2020-01-08 19:32:10 -0500

    Initial Commit

---
+++ exercise1iossem2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  exercise1iossem2
+//
+//  Created by Simran Chakkal on 2020-01-08.
+//  Copyright © 2020 Simran Chakkal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
